1f3979307d1bb5b72f5b2ad04a3535f872c9e488
ich777 <christophhummer@gmail.com>
2022-03-22 10:36:04 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -19,19 +19,19 @@ screen -wipe 2&>/dev/null
 
 echo "---Resolution check---"
 if [ -z "${CUSTOM_RES_W} ]; then
-	CUSTOM_RES_W=1024
+	CUSTOM_RES_W=1280
 fi
 if [ -z "${CUSTOM_RES_H} ]; then
-	CUSTOM_RES_H=768
+	CUSTOM_RES_H=850
 fi
 
-if [ "${CUSTOM_RES_W}" -le 1023 ]; then
+if [ "${CUSTOM_RES_W}" -le 1279 ]; then
 	echo "---Width to low must be a minimal of 1024 pixels, correcting to 1024...---"
-    CUSTOM_RES_W=1024
+    CUSTOM_RES_W=1280
 fi
-if [ "${CUSTOM_RES_H}" -le 767 ]; then
+if [ "${CUSTOM_RES_H}" -le 849 ]; then
 	echo "---Height to low must be a minimal of 768 pixels, correcting to 768...---"
-    CUSTOM_RES_H=768
+    CUSTOM_RES_H=850
 fi
 
 echo "---Starting TurboVNC server---"
@@ -44,9 +44,6 @@ echo "---Starting noVNC server---"
 websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem ${NOVNC_PORT} localhost:${RFB_PORT}
 sleep 2
 
-echo "---Container under construction!---"
-sleep infinity
-
 echo "---Starting jdupes-gui---"
 cd ${DATA_DIR}
-/usr/bin/jdupes-gui
+/usr/bin/jdupes-gui 2>/dev/null
